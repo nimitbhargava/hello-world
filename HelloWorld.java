51472ce6e1bfa3bafd2e08fb1e3528586829f65f
Nimit Bhargava <nimitbhargava@gmail.com>
2016-09-18 16:51:07 +0530

    changes in java hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 class HelloWorld {
     public static void main(String[] args) {
       String hello = "Hello ";
-      int i = 2109;
-        System.out.println(hello+i);
+      int i = 2109+10;
+      int j = 10;
+        System.out.println(hello+i+j);
     }
 }
